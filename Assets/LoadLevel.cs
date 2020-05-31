4c4b2465d844fbf124efc298f7175ac67ef1dc98
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-05-31 23:43:26 +0200

    Add Music, load First scene after Splash

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] float delay = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadMainLevel());    
+    }
+
+    private IEnumerator LoadMainLevel()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(1);
+    }
+}
